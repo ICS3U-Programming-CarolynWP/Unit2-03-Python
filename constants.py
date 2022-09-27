7848d497adbb4dfa91ecb472d4017c2133b108b5
Carolyn Webster Pless <carolyn.webster-pless@stu.ocsb.ca>
2022-09-27 22:58:15 +0000

    forgot to use black on constants.py

--- constants.py
+++ constants.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-#Created by: Carolyn Webster Pless
-#Created on: 2022/09/27
-#Tau constant for main.py
+# Created by: Carolyn Webster Pless
+# Created on: 2022/09/27
+# Tau constant for main.py
 
-#constant
+# constant
 TAU = 6.28
